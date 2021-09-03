4e357e8f26d5acf3c4f5daeb65c4acff6ef9ed83
Parker Lockhart <parkerlockhart42@gmail.com>
2021-09-03 11:46:16 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Fifth Element"
+is_entertaining = true
+movie = "Moonlight"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["popcorn", "blueberries", "dark chocolate"]
+favorite_foods.shift()
+puts movie.length
