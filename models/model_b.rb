65ae4951b7824b074e3d20fb84d6f5322cf5bef1
Parker Lockhart <parkerlockhart42@gmail.com>
2021-09-03 12:17:38 -0500

    Create developer hash model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: "Parker Lockhart",
+  type: "Back End",
+  years_of_experience: 2,
+  salary: 80000.00,
+  awesome: true,
+  working_remote: true,
+  languages_used: ["Ruby", "Python", "PHP"],
+  current_projects: ["API Build", "Database bugfix"]
+}
