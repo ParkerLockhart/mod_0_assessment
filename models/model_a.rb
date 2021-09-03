b65401712495ce50bc4a8d018d8b04994f5fb2cf
Parker Lockhart <parkerlockhart42@gmail.com>
2021-09-03 12:06:15 -0500

    Create dinosaur hash model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  common_name: "Plesiosaurus",
+  period: "Jurassic",
+  average_length_in_feet: 11,
+  year_named: 1821,
+  live_birth: true,
+  carnivorous: true,
+  diet: ["clams", "snails", "fish"],
+  close_relatives: ["P. brachypterygius", "P. guilielmiimperatoris", "P. tournemirensis"]
+}
